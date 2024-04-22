c137a22f3223e743cedc5590c7e31e6c5019d57f
analiajaime <134345036+analiajaime@users.noreply.github.com>
2024-04-22 18:08:55 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
 // Se conecta a la BD
-mongoose.connect("mongodb+srv://analiajaime:xxxx.ckbgdx2.mongodb.net/ecommerce?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://qjorgefabian:kW6XM4el1Aa3BePX@cluster0.poak6p5.mongodb.net/")
     .then(() => console.log("Connection to db successful"))
     .catch((error) => console.log("Error establishing connection to db:", error))
